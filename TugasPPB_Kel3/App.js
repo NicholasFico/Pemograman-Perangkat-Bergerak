5e518b31489f979227e59a03fab47847ee615574
NicholasFico <92150095+NicholasFico@users.noreply.github.com>
2022-04-15 18:20:51 +0700

    Add files via upload

---
+++ TugasPPB_Kel3/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import HomeScreen from './components/HomeScreen'
+
+export default function App() {
+    return (
+        <HomeScreen />
+    );
+}
+
---
+++ TugasPPB_Kel3/components/ScoreScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, View} from 'react-native'
+
+const ScoreScreen = () =>{
+    return (
+        <View>
+            <Text>ScoreScreen</Text>
+        </View>
+    )
+}
+
+export default ScoreScreen;
